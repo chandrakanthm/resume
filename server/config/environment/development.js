7c63608063e43ece9c884bc9451c9af3b7d22bd5
chandrakanth <chandra619kanth@gmail.com>
2016-10-29 12:41:56 -0700

    modify mdlab

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -3,10 +3,11 @@
 // Development specific configuration
 // ==================================
 module.exports = {
-  // MongoDB connection options
-  mongo: {
-    uri: 'mongodb://localhost/resume-dev'
-  },
+    // MongoDB connection options
+    mongo: {
+        //uri: 'mongodb://localhost/resume-dev'
+        uri: 'mongodb://Chandra619kanth:Mcmacha#1@ds041486.mlab.com:41486/resume'
+    },
 
-  seedDB: true
+    seedDB: true
 };
